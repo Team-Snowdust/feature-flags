c25e60a1041b0a6e5a5a31ca20c47bb4a5e8028f
Lucas Gangstad <lucas@lasttalon.dev>
2023-06-20 11:17:42 -0500

    Add basic API structure

---
+++ lib/init.lua
@@ -0,0 +1,22 @@
+--[=[
+	@class FeatureFlags
+]=]
+
+return {
+	-- create = create,
+	-- read = read,
+	-- update = update,
+	-- retire = retire,
+	-- destroy = destroy,
+
+	-- get = get,
+	-- onChange = onChange,
+	-- isActive = isActive,
+
+	-- init = init,
+	-- config = config,
+	-- reset = reset,
+
+	-- getAllFlags = getAllFlags,
+	-- getAuditLog = getAuditLog,
+}
