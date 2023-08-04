617dc4884e4e6dfa37a3e72e663455fe9ffbdbf9
Lucas Gangstad <lucas@lasttalon.dev>
2023-08-04 07:39:52 -0500

    Add Flag docs

--- lib/createFlag.lua
+++ lib/createFlag.lua
@@ -4,11 +4,17 @@ local Signal = require(script.Parent.Signal)
 local isActive = require(Package.isActive)
 
 --[=[
+	Represents an individual flag.
+
+	This provides convenience access to other library functions with this flag
+	already provided. See [FeatureFlags] for more information about each function.
+
 	@class Flag
 ]=]
 export type Flag = {
 	name: string,
-	create: (config: Flags.FlagConfig) -> (),
+	create: (config: Flags.FlagConfig, retired: boolean?) -> (),
+	exists: () -> boolean,
 	read: () -> Flags.FlagData,
 	update: (config: Flags.FlagConfig) -> (),
 	retire: (retired: boolean?) -> (),
@@ -18,6 +24,119 @@ export type Flag = {
 }
 
 --[=[
+	The name of this flag.
+
+	@prop name string
+	@within Flag
+	@readonly
+]=]
+
+--[=[
+	Creates a new flag with the provided configuration.
+
+	:::caution
+	Receiving a Flag object generally indicates that a flag already exists. The
+	`create` function should only be used to create flags that don't already
+	exist.
+	:::
+
+	See: [create](FeatureFlags#create)
+
+	@param config FlagConfig
+	@param retired? boolean -- Whether this flag is retied
+
+	@error "Flag '%s' already exists." -- Thrown when a flag with this name already exists.
+
+	@function create
+	@within Flag
+]=]
+
+--[=[
+	Checks if this flag currently exists.
+
+	See: [exists](FeatureFlags#exists)
+
+	@return boolean
+
+	@function exists
+	@within Flag
+]=]
+
+--[=[
+	Reads the data of this flag.
+
+	See: [read](FeatureFlags#read)
+
+	@return FlagData
+
+	@function read
+	@within Flag
+]=]
+
+--[=[
+	Updates the configuration of this flag.
+
+	See: [update](FeatureFlags#update)
+
+	@param config FlagConfig
+
+	@function update
+	@within Flag
+]=]
+
+--[=[
+	Sets the retired status of this flag.
+
+	See: [retire](FeatureFlags#retire)
+
+	@param retired? boolean
+
+	@function retire
+	@within Flag
+]=]
+
+--[=[
+	Removes this flag entirely.
+
+	See: [destroy](FeatureFlags#destroy)
+
+	@function destroy
+	@within Flag
+]=]
+
+--[=[
+	Checks if a feature flag is active based on provided context and
+	configuration.
+
+	See: [isActive](FeatureFlags#isActive)
+
+	@param context? ActivationContext
+	@param config? ActivationConfig
+
+	@return boolean
+
+	@function isActive
+	@within Flag
+]=]
+
+--[=[
+	Subscribe to the changed event for this flag.
+
+	This callback is only invoked when this flag is changed.
+
+	See: [Changed](FeatureFlags#Changed)
+
+	@param callback (record: ChangeRecord) -> ()
+
+	@return Connection
+
+	@function onChange
+	@within Flag
+]=]
+
+--[=[
+	Creates a new flag object.
+
 	@within Flag
 	@ignore
 ]=]
@@ -25,44 +144,30 @@ local function createFlag(name: string): Flag
 	return table.freeze({
 		name = name,
 
-		--[=[
-			@within Flag
-		]=]
-		create = function(config: Flags.FlagConfig)
-			Flags.create(name, config)
+		create = function(config: Flags.FlagConfig, retired: boolean?)
+			Flags.create(name, config, retired)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
+		exists = function(): boolean
+			return Flags.exists(name)
+		end,
+
 		read = function(): Flags.FlagData
 			return Flags.read(name)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
 		update = function(config: Flags.FlagConfig)
 			Flags.update(name, config)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
 		retire = function(retired: boolean?)
 			Flags.retire(name, retired)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
 		destroy = function()
 			Flags.destroy(name)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
 		isActive = function(
 			context: isActive.ActivationContext?,
 			config: isActive.ActivationConfig?
@@ -70,9 +175,6 @@ local function createFlag(name: string): Flag
 			return isActive(name, context, config)
 		end,
 
-		--[=[
-			@within Flag
-		]=]
 		onChange = function(callback: (record: Flags.ChangeRecord) -> ()): Signal.Connection
 			return Flags.Changed:Connect(function(changedName: string, record: Flags.ChangeRecord)
 				if changedName == name then
